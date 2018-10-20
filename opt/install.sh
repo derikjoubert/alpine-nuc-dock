52849360ff3735caf3dfcc9757783bbb2283178a
Derik Joubert <derikjoubertmx@gmail.com>
2018-10-20 18:24:07 +0200

    Update opt/install.sh

--- opt/install.sh
+++ opt/install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 apk add samba samba-common-tools
+mv opt/ ..
 chmod 0777 /opt
 cp /opt/samba/smb.conf /etc/samba/smb.conf
+rm -rf alpine-nuc-dock
 # adduser admin
 #smbpasswd
 (echo password; echo password) | smbpasswd -a
